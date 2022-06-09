5fe8a44d71626299686ba061cfae7c972dffeecf
tulsiwartiz <tulsii@gmail.com>
2022-06-09 14:51:30 +0530

    employee consent changes

--- employees/reports/consent_letter/ajax/update_temp_data_in_entities_data.php
+++ employees/reports/consent_letter/ajax/update_temp_data_in_entities_data.php
@@ -5,18 +5,17 @@
 	include(DIR.'files/functions.php');
 
 
-	echo "UPDATE " . $cid . "_sys_settings ' SET c_company_temp = '" . $_REQUEST['field1'] . "', c_address_temp = '" . $_REQUEST['field2'] . "', c_function_temp = '" . $_REQUEST['field3'] . "', c_telephone_temp = '" . $_REQUEST['field4'] . "', c_email_temp = '" . $_REQUEST['field5'] . "'";
+;
 
-	die();
+    $sql3244 = "UPDATE ".$cid. "_sys_settings SET c_company_temp = '".$_REQUEST['field1']. "', c_address_temp = '".$_REQUEST['field2']. "', c_function_temp = '" . $_REQUEST['field3'] . "', c_telephone_temp = '" . $_REQUEST['field4']. "', c_email_temp = '" . $_REQUEST['field5'] . "' WHERE id= '1' ";
 
-    $sql3244 = "UPDATE ".$cid. "_sys_settings ' SET c_company_temp = '".$_REQUEST['field1']. "', c_address_temp = '".$_REQUEST['field2']. "', c_function_temp = '" . $_REQUEST['field3'] . "', c_telephone_temp = '" . $_REQUEST['field4']. "', c_email_temp = '" . $_REQUEST['field5'] . "'";
-    // $dbc->query($sql3244);
+    $dbc->query($sql3244);
 
 		
 	
 
 
 
-// ob_clean();
-// echo "success";
-// exit;
+ob_clean();
+echo "success";
+exit;
