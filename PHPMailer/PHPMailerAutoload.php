453485af75dcb61416aa3220ecdf19a49d1d03b1
tulsiwartiz <84794971+tulsiwartiz@users.noreply.github.com>
2022-08-04 18:31:50 +0530

    Update PHPMailerAutoload.php

--- PHPMailer/PHPMailerAutoload.php
+++ PHPMailer/PHPMailerAutoload.php
@@ -17,10 +17,7 @@
  * FITNESS FOR A PARTICULAR PURPOSE.
  */
 
-/**
- * PHPMailer SPL autoloader.
- * @param string $classname The name of the class to load
- */
+
 function PHPMailerAutoload($classname)
 {
     //Can't use __DIR__ as it's only in PHP 5.3+
